9ccdb871f43c4664ef4dd2b0dfdbe3f5df781c02
Chris L <chris.ladden@gmail.com>
2022-04-24 11:47:20 -0500

    added initial code for setTimeout function practice

--- move.js
+++ move.js
@@ -1,12 +1,29 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
 
-    return {
-        to: moveToCoordinates
-    }
+  return {
+    to: moveToCoordinates,
+  };
+}
+
+// initial code for practicing setTimeout
+function east() {
+  moveToCoordinates(500, 250);
+}
+
+function west() {
+  moveToCoordinates(100, 650);
+}
+
+function north() {
+  moveToCoordinates(500, 650);
+}
+
+function south() {
+  moveToCoordinates(100, 250);
 }
